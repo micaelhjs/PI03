b4398bc7975985018b9b0242d908c27760684cd6
micaelhjs <micaelhjs>
2024-05-26 13:35:02 -0300

    Atualizações paginas

--- FarmSystem/paginas/urls.py
+++ FarmSystem/paginas/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import IndexView
+from django.urls import path, include
+from .views import IndexView, Quem_SomosView, ContatosView
+from .views import grafico
 
-urlpatterns = {
-    path('', IndexView.as_view(), name= 'inicio'),
-}
+urlpatterns = [
+    path('', IndexView.as_view(), name= 'index'),
+    path('quem_somos/', Quem_SomosView.as_view(), name= 'quem_somos'),
+    path('contatos/', ContatosView.as_view(), name= 'contatos'),
+    path('grafico/', grafico, name='grafico'),
+]
--- FarmSystem/paginas/views.py
+++ FarmSystem/paginas/views.py
@@ -1,6 +1,27 @@
 from django.views.generic import TemplateView
 
-# Criar as views aqui
+from django.shortcuts import render
+from sensores.models import SensorData
 
 class IndexView(TemplateView):
-    template_name = "modelo.html"
+    template_name = "index.html"
+
+class Quem_SomosView(TemplateView):
+    template_name = "quem_somos.html"
+
+class ContatosView(TemplateView):
+    template_name = "contatos.html"
+
+def grafico(request):
+    sensor_data = SensorData.objects.all().order_by('timestamp')
+    temperatures = [data.temperature for data in sensor_data]
+    humidities = [data.humidity for data in sensor_data]
+    timestamps = [data.timestamp.strftime("%Y-%m-%d %H:%M:%S") for data in sensor_data]
+
+    context = {
+        'temperatures': temperatures,
+        'humidities': humidities,
+        'timestamps': timestamps,
+    }
+    return render(request, 'grafico.html', context)
+
